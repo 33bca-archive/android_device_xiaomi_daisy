a047703531ffa840d4dbc014ca05bdcf372222bf
Nicholas Lim <nicknitewolf@lineageos.org>
2018-07-22 20:16:41 +0800

    daisy: Fix up A/B support
     * bootctrl from https://github.com/LineageOS/android_hardware_qcom_bootctrl
     * gpt-utils from https://github.com/LineageOS/android_device_xiaomi_tissot
    
    Change-Id: Id28522337507ddda3fd5ab539130b33e9fc70b29

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,6 +50,9 @@ TARGET_HW_DISK_ENCRYPTION := true
 ENABLE_CPUSETS := true
 ENABLE_SCHEDBOOST := true
 
+# GPT Utils
+BOARD_PROVIDES_GPTUTILS := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 veritykeyid=id:5560e7863b4d8118c2f1b065595cf93bb2447992
--- omni_daisy.mk
+++ omni_daisy.mk
@@ -25,10 +25,50 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit language packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    system
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    bootctrl.msm8953 \
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.msm8953 \
+    libcutils \
+    libgptutils \
+    libz
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images \
     charger
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_daisy
