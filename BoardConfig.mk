58fe0f846bf78ad4cdf63ca01bf93866ce041d0d
Offain <fabian.leutenegger@bluewin.ch>
2019-02-24 19:38:39 +0100

    daisy: Import TWRP installer script for A/B devices
     * Needs the following patch: https://gerrit.omnirom.org/#/c/android_build/+/33182/
     * Also add prebuilt unzip because toolbox doesn't support it...
    
    Change-Id: If29f8cbd8bab77906d19da87c084696573ddb592

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,5 +104,9 @@ TW_EXCLUDE_DEFAULT_USB_INIT := true
 TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
 TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT}/system/lib64/android.hardware.boot@1.0.so
 
+# Recovery Installer
+USE_RECOVERY_INSTALLER := true
+RECOVERY_INSTALLER_PATH := $(LOCAL_PATH)/installer
+
 # Security Patch Hack to prevent Anti Rollback
 PLATFORM_SECURITY_PATCH := 2018-07-01
