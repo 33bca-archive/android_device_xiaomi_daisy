ffa91ea8329874c3f59ecab845d6712a14d50084
Touka-chan <toukachan@mailbox.org>
2019-04-22 21:25:36 +0200

    Disabled HW overlays, various fixes for Butterfly kernel
    Change-Id: I17413960b28ff0fe0f808d16af92fdbdab2b6653

--- device-props.mk
+++ device-props.mk
@@ -1,3 +1,6 @@
 # Setting the DPI value that is actually sane
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.sf.lcd_density=401
+# Always use GPU for screen compositing
+PRODUCT_PROPERTY_OVERRIDES += \
+	debug.sf.disable_hwc=1
