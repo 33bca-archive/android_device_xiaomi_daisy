26e1a1a6c8ea1ecc6a9a85fde6abe0c92041a9bf
Offain <fabian.leutenegger@bluewin.ch>
2019-05-02 23:14:26 +0200

    daisy: Add system properties
     * Extracted from stock rom
    
    Change-Id: I515ba0da51d888b02662aa049e2e4f7a335e4d6e

--- device-props.mk
+++ device-props.mk
@@ -1,1 +1,103 @@
-# TODO
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.hfp.client=1
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.display.lmax=1280x720 \
+    persist.vendor.camera.display.umax=1920x1080 \
+    vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
+    vendor.camera.lowpower.record.enable=1 \
+    vidc.enc.dcvs.extra-buff-count=2
+
+# CNE/DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dpm.feature=1 \
+    persist.vendor.cne.feature=1
+
+# Coresight
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.coresight.config=stm-events
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.egl.hw=0 \
+    debug.enable.sglscale=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    debug.mdpcomp.logs=0 \
+    debug.sf.enable_hwc_vds=1 \
+    debug.sf.hw=0 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.recomputecrop=0 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.debug.wfd.enable=1 \
+    persist.demo.hdmirotationlock=false \
+    persist.hwc.enable_vds=1 \
+    persist.hwc.mdpcomp.enable=true \
+    ro.qualcomm.cabl=2 \
+    ro.vendor.display.cabl=2 \
+    sdm.debug.disable_skip_validate=1 \
+    vendor.display.disable_skip_validate=1 \
+    vendor.display.enable_default_color_mode=1 \
+    vendor.gralloc.enable_fb_ubwc=1
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.qfp=false
+
+# FM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fm.transmitter=false
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    av.debug.disable.pers.cache=1 \
+    media.aac_51_output_enabled=true \
+    media.msm8956hw=0 \
+    mm.enable.smoothstreaming=true \
+    mmp.enable.3g2=true \
+    vendor.mm.enable.qcom_parser=1048575 \
+    vendor.vidc.dec.downscalar_height=1088 \
+    vendor.vidc.dec.downscalar_width=1920 \
+    vendor.vidc.disable.split.mode=1 \
+    vendor.vidc.enc.disable.pq=true \
+    vendor.vidc.enc.disable_bframes=1 \
+    vendor.video.disable.ubwc=1
+
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true
+
+# Netmgrd
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.use_data_netmgrd=true \
+    persist.data.netmgrd.qos.enable=true \
+    persist.vendor.data.mode=concurrent
+
+# Nitz
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3=""
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
+    ril.subscription.types=NV,RUIM \
+    rild.libargs=-d/dev/smd0 \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ro.telephony.default_network=22,20
+
+# Time Services
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.use_fifo_ui=1
