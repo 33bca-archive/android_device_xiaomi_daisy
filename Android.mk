43c8cabec81e862eda070439d522909e253cf084
Offain <fabian.leutenegger@bluewin.ch>
2019-05-06 07:06:04 +0200

    daisy: Update to Pie
     * Kanged most stuff from "https://github.com/TeamWin/android_device_xiaomi_tissot"
    
    Change-Id: I9307896fa91e51ffb64aa75949eac87bdbf8b683

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The Android Open Source Project
+# Copyright (C) 2019 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,5 +14,8 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_daisy.mk
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE), daisy)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
