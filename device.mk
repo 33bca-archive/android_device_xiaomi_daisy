aa18f903630a68182735755a09c960b5d83039fc
Offain <fabian.leutenegger@bluewin.ch>
2019-05-18 18:17:20 +0200

    daisy: Build QCOM power HAL
    Change-Id: I199e8e8592a7c0f77473af4a12ffc91aac22e8dc

--- device.mk
+++ device.mk
@@ -88,6 +88,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     lineage.livedisplay@2.0-service-sdm
 
+# Power
+PRODUCT_PACKAGES += \
+    power.qcom
+
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
