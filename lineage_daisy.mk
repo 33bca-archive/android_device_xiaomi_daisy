1e5a0d76c0849f66ee71b30215445ac9f640d2d3
Offain <fabian.leutenegger@bluewin.ch>
2019-06-03 02:55:44 +0200

    daisy: Update build fingerprint
     * From stock V10.0.9.0 update
    
    Change-Id: I5c7e2cab5cb3b0b1fb4ba436a770c7627e935e6d

--- lineage_daisy.mk
+++ lineage_daisy.mk
@@ -34,11 +34,11 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="daisy" \
-    PRODUCT_NAME="daisy" \
-    PRIVATE_BUILD_DESC="daisy-user 9 PKQ1.180917.001 V10.0.1.0.PDLMIFJ release-keys"
+    TARGET_DEVICE="daisy_sprout" \
+    PRODUCT_NAME="daisy_sprout" \
+    PRIVATE_BUILD_DESC="daisy-user 9 PKQ1.180917.001 V10.0.9.0.PDLMIXM release-keys "
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop	
-BUILD_FINGERPRINT := "xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.9.0.PDLMIXM:user/release-keys"
 
 TARGET_VENDOR := Xiaomi
