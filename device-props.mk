c3549702e8c6a4e26ebb51ceee93c42d794ac3e2
pawelik001 <pawelik001@gmail.com>
2019-08-10 11:00:49 +0000

    daisy: Enable VoLTE

--- device-props.mk
+++ device-props.mk
@@ -105,3 +105,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # UI
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.use_fifo_ui=1
+
+# VoLTE Enable
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1
+    persist.dbg.volte_avail_ovr=1
+    persist.dbg.vt_avail_ovr=1
+    persist.dbg.wfc_avail_ovr=1
+    persist.radio.rat_on=combine
+    persist.radio.data_ltd_sys_ind=1
+    persist.radio.data_con_rprt=1
+    persist.radio.calls.on.ims=1
